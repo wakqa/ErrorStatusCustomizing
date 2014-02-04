81906938392ed07a74f97f883eb90d32ef4b42eb
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-02-04 09:19:33 +0000

    Update #4

---
+++ ErrorStatusCustomizing/bootstrap.js
@@ -0,0 +1,9 @@
+﻿
+addHttpRequestHandler('/provokeError500','script.js','provokeError500');
+addHttpRequestHandler('/provokeError501','script.js','provokeError501');
+addHttpRequestHandler('/provokeError401','script.js','provokeError401');
+addHttpRequestHandler(
+      '/echoBody/',              
+      'script.js',  
+      'echoBody'         
+);
---
+++ ErrorStatusCustomizing/script.js
@@ -0,0 +1,23 @@
+﻿function provokeError500() {
+	
+	elfe("jfejj");
+}
+
+function provokeError501(request, response) {
+	
+	var xhr = new XMLHttpRequest();
+	xhr.open("WHATEVER","/");
+	xhr.send(null);
+	return xhr.responseText;
+}
+	
+	
+function provokeError401(request, response) {
+	
+	return "ok";
+}	
+function echoBody(request, response){
+	var body = request.body;
+	console.log(body);
+	return body;
+}
